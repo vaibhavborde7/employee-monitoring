66cde9aefc69587061459691743c7dff65324e43
Vaibhav Borde <142778795+vaibhavborde7@users.noreply.github.com>
2024-10-28 00:42:58 +0530

    Update Header.jsx

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -14,7 +14,7 @@ export default function Home() {
             <h1>Home</h1>
             <h2>About Us</h2>
             <h3>Contact</h3>
-            <Link href="http://localhost:3000/admin" passHref>
+            <Link href="/admin" passHref>
               <button className="relative text-white bg-[#4eb6f7] rounded-lg border-[#4eb6f7] px-4 py-2 transition transform hover:scale-105 hover:shadow-lg active:scale-95 focus:outline-none focus:ring-2 focus:ring-blue-400">
                 Sign Up
               </button>
